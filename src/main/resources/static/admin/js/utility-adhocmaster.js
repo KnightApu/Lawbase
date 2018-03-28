12e1e7e7ebac3b4c3fd6a3e17ac46167283a0f8d
Mahmud Hassan Apu <knight.apu@gmail.com>
2018-03-28 16:30:09 +0600

    Revert "Error Modal"
    This reverts commit b6bcdb613059249634693230ab8c0e2cd6a9da2e.

--- src/main/resources/static/admin/js/utility-adhocmaster.js
+++ src/main/resources/static/admin/js/utility-adhocmaster.js
@@ -190,14 +190,6 @@ mApp.showSuccessModal = function ( successMessage ) {
 	
 }
 
-mApp.showDeleteModal = function (successMessage) {
-	
-	$('#delete-modal .modal-body').html( successMessage +". <i>Please reload the page.</i>");
-	
-	$('#delete-modal').modal();
-	
-}
-
 mApp.redirect = function ( url, timeout ) {
 	
 	setTimeout( function() {
