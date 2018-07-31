af1211838c020c95194b3555d343e0016a946e64
farin <ffarin09@gmail.com>
2018-07-31 10:55:36 +0600

    static resource cached

--- src/main/java/com/lawbase/config/MvcConfig.java
+++ src/main/java/com/lawbase/config/MvcConfig.java
@@ -1,8 +1,12 @@
 package com.lawbase.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -28,6 +32,24 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
          resolver.setSuffix("");
          return resolver;
     }*/
+	
+	@Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	 
+		 registry.addResourceHandler( "/**" )
+		 		.addResourceLocations( "classpath:/static/" )
+		 		.setCacheControl( CacheControl.maxAge( 1, TimeUnit.DAYS ) );
+		 
+		 registry.addResourceHandler( "/admin/js/**" )
+		 		.addResourceLocations( "classpath:/static/admin/js/" )
+		 		.setCacheControl( CacheControl.maxAge( 5, TimeUnit.MINUTES ) );
+		 
+		 registry.addResourceHandler( "/public/js/**" )
+	 			.addResourceLocations( "classpath:/static/public/js/" )
+	 			.setCacheControl( CacheControl.maxAge( 5, TimeUnit.MINUTES ) );
+		 
+	 
+	 }
 
 
 }
