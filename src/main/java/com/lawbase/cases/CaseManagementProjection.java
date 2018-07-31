fea0af9783f54c7253939e9fb7efd97856df3561
farin <ffarin09@gmail.com>
2018-07-31 10:26:50 +0600

    case management projection done

--- src/main/java/com/lawbase/cases/CaseManagementProjection.java
+++ src/main/java/com/lawbase/cases/CaseManagementProjection.java
@@ -1,10 +1,12 @@
 package com.lawbase.cases;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
+@Document( collection = "case" )
 public class CaseManagementProjection {
 
 private final String title, status;
@@ -12,18 +14,18 @@ private final String title, status;
 	@JsonSerialize(using = ToStringSerializer.class)
 	private final ObjectId id;
 	
-	private final String journalTitle, volume;
+	private final String courtBookTitle, volume;
 	
 	private final int year;
 	
 	
-	CaseManagementProjection( ObjectId id, String title, String status, String journalTitle, int year, String volume ) {
+	CaseManagementProjection( ObjectId id, String title, String status, String courtBookTitle, int year, String volume ) {
 		
 		super();
 		this.id = id;
 		this.title = title;
 		this.status = status;
-		this.journalTitle = journalTitle;
+		this.courtBookTitle = courtBookTitle;
 		this.year = year;
 		this.volume = volume;
 		
@@ -53,9 +55,9 @@ private final String title, status;
 		
 	}
 	
-	public String getJournalTitle() {
+	public String getcourtBookTitle() {
 		
-		return this.journalTitle;
+		return this.courtBookTitle;
 		
 	}
 
